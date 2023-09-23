9b9fdb825c4fd0d0ba5056b4b836b4fde4d0f966
HarishKarthik0376 <103718125+HarishKarthik0376@users.noreply.github.com>
2023-09-23 12:57:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,23 +28,20 @@ let additem = (input) =>{
 }
 document.getElementById("two").addEventListener("click",function(){
     let listitem = document.createElement("li");
-    if(input.value.length == 0)
-    {
-        listitem.appendChild(lisitem)==false;
-    }
     listitem.innerHTML = input.value +`<i>X</i>`;
     listitem.querySelector("i").style.fontSize = "1.3rem";
     listitem.querySelector("i").style.textAlign = "centre";
     listitem.querySelector("i").style.color = "white";
     listitem.querySelector("i").style.fontStyle = "nomral";
     listitem.querySelector("i").style.fontFamily = "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif";
-    list.appendChild(listitem);
-    input.value = "";
     listitem.querySelector("i").addEventListener("click",function(){
         listitem.remove();
     })
     listitem.addEventListener("click",function(){
         this.classList.toggle("done");
     })
-    list.appendChild(listitem);
+    if(input.value.length!=0){
+        list.appendChild(listitem);
+        input.value = "";
+    }
 })
